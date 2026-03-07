1371f844a764dbf8a3ec69d66ed0ad03882abf26
KoolKid6958 <koolkid@koolkid6958.dev>
2026-03-07 17:45:14 -0500

    chore: remove all rust files
    Signed-off-by: KoolKid6958 <koolkid@koolkid6958.dev>

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-use pyo3::prelude::*;
-
-mod csv_handler;
-
-#[pymodule]
-fn rustlibs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(csv_handler::get_csv_files_from_folder, m)?)?;
-    Ok(())
-}
